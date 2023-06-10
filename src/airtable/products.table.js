719e33935dbdc933300c6cf0cb49faf211b82151
JSHayk <jshaykdeveloper@gmail.com>
2023-06-10 18:27:51 +0400

    ssss

--- src/airtable/products.table.js
+++ src/airtable/products.table.js
@@ -5,23 +5,30 @@ import invalidArguments from "../helpers/invalidArguments.js";
 
 const { products_token } = config.airtable;
 
+
 async function getRecords() {
   try {
     const records = await client.getTableByName(products_token);
-    console.log(records, "records")
+    console.log(records, "records");
     if (records) {
-      const modifyedRecords = records.map((record) => {
+      const modifiedRecords = records.map((record) => {
         if (Object.keys(record).length !== 0) {
           return modifyRecord(record);
         }
       });
-      return modifyedRecords;
+      return modifiedRecords;
     }
     return [];
   } catch (err) {
     throw new Error(err);
   }
 }
+
+
+
+
+
+
 
 function modifyRecord(record) {
   invalidArguments([record]);

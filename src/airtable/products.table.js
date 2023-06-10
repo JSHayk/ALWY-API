eb8049b23dd667d6378f22b3ec3d82df2b2092aa
JSHayk <jshaykdeveloper@gmail.com>
2023-06-10 18:13:55 +0400

    asd

--- src/airtable/products.table.js
+++ src/airtable/products.table.js
@@ -8,12 +8,15 @@ const { products_token } = config.airtable;
 async function getRecords() {
   try {
     const records = await client.getTableByName(products_token);
-    const modifyedRecords = records.map((record) => {
-      if (Object.keys(record).length !== 0) {
-        return modifyRecord(record);
-      }
-    });
-    return modifyedRecords;
+    console.log(records, "records")
+    if (records) {
+      const modifyedRecords = records.map((record) => {
+        if (Object.keys(record).length !== 0) {
+          return modifyRecord(record);
+        }
+      });
+      return modifyedRecords;
+    }
   } catch (err) {
     throw new Error(err);
   }
